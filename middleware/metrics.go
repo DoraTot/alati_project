3f62dc0d3a7d9bac4d3ca9694537d7c735ce8d06
MiodragUgricaFTN <ugrica.sr7.2022@uns.ac.rs>
2024-06-27 12:08:13 +0200

    Fixed yml

--- middleware/metrics.go
+++ middleware/metrics.go
@@ -23,40 +23,59 @@ type ResponseWriter struct {
 }
 
 func (r *ResponseWriter) WriteHeader(status int) {
+	log.Printf("Setting response status code to: %d", status)
 	r.statusCode = status
 	r.ResponseWriter.WriteHeader(status)
 }
 
 func (m *Metrics) Count(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Scrapping...")
+		log.Println("Request received for:", r.URL.Path)
 		start := time.Now()
 
 		route := mux.CurrentRoute(r)
-		path, _ := route.GetPathTemplate()
+		if route == nil {
+			log.Println("No current route found for request")
+		}
+
+		path, err := route.GetPathTemplate()
+		if err != nil {
+			log.Printf("Error getting path template: %v", err)
+		}
+		log.Printf("Path template: %s", path)
 
 		rw := &ResponseWriter{w, http.StatusOK}
 		next.ServeHTTP(rw, r)
 
 		duration := time.Since(start).Seconds()
+		log.Printf("Processed request for %s in %f seconds", path, duration)
 
 		statusCode := rw.statusCode
+		log.Printf("Response status code: %d", statusCode)
+
 		if statusCode >= 200 && statusCode < 400 {
 			m.service.HttpSuccessfulRequests.WithLabelValues().Inc()
+			log.Println("Incremented successful requests counter")
 		} else if statusCode >= 400 && statusCode < 600 {
 			m.service.HttpUnsuccessfulRequests.WithLabelValues().Inc()
+			log.Println("Incremented unsuccessful requests counter")
 		}
 
 		m.service.HttpTotalRequests.WithLabelValues().Inc()
+		log.Println("Incremented total requests counter")
 		m.service.AverageRequestDuration.WithLabelValues(r.Method, path).Set(duration)
+		log.Printf("Set average request duration for %s %s", r.Method, path)
 		m.service.RequestsPerTimeUnit.WithLabelValues(r.Method, path, "seconds").Inc()
+		log.Printf("Incremented requests per time unit for %s %s", r.Method, path)
 	})
 }
 
 func (m *Metrics) MetricsHandler() http.Handler {
+	log.Println("Metrics handler invoked")
 	return promhttp.HandlerFor(m.service.Registry, promhttp.HandlerOpts{})
 }
 
 func AdaptPrometheusHandler(handler http.Handler, metrics *Metrics) http.Handler {
+	log.Println("Adapting handler with Prometheus metrics")
 	return metrics.Count(handler)
 }
